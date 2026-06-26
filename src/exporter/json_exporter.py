982c61c1390ad176605009be1b81a7aa72473960
Vishwanath <vishwasharma300@gmail.com>
2026-06-27 02:10:24 +0530

    feat: add JSON report exporter

---
+++ src/exporter/json_exporter.py
@@ -0,0 +1,13 @@
+"""
+JSON Exporter
+"""
+
+import json
+
+def export_json(results):
+
+    with open("reports/report.json", "w") as file:
+
+        json.dump(results, file, indent=4)
+
+    print("JSON report generated.")
